2d6dd0e6a6a16f8fb9e7d16a227e870d1f868274
kevin <kevincantstop@gmail.com>
2024-09-28 13:00:24 +0800

    added more ops

--- src/app/api/(auth)/users/route.js
+++ src/app/api/(auth)/users/route.js
@@ -12,3 +12,29 @@ export const POST = async (req) => {
 
     return json({ user });
 }
+
+export const PATCH = async (req) => {
+    const body = await req.json();
+    const user = await User.findByPk(body.id)
+
+    if (user) {
+        delete body.id;
+
+        const updated = await User.update({ ...body }, {
+            where: { id: user.id }
+        })
+        return json({ ok: true, affected: updated })
+    }
+    return json({ message: `User: ${body.id} not found` }, 404)
+}
+
+export const DELETE = async (req) => {
+    const body = await req.json();
+    const user = await User.findByPk(body.id)
+
+    if (user) {
+        await User.destroy({ where: { id: body.id } })
+        return json({ ok: true })
+    }
+    return json({ message: `User: ${body.id} not found` }, 404)
+}
