c1832b6447b47eb56d2138abb1b3a44e7bb827fb
kevin <kevincantstop@gmail.com>
2024-09-29 00:48:33 +0800

    fixed issue

--- src/app/api/(dashboard)/posts/route.js
+++ src/app/api/(dashboard)/posts/route.js
@@ -1,5 +1,5 @@
 import { json } from "@/core";
-import { Post } from "@/core/models";
+import { Post, User, Category } from "@/core/models";
 
 export const GET = async () => {
     const posts = await Post.findAll();
@@ -8,6 +8,24 @@ export const GET = async () => {
 
 export const POST = async (req) => {
     const body = await req.json();
+    const associate = {};
+
+    if (body.authorId) {
+        const user = await User.findByPk(body.authorId)
+        if (user == null) {
+            return json({ message: `Author: ${body.authorId} not found.` }, 404);
+        }
+        associate.user = user;
+    }
+
+    if (body.categoryId) {
+        const category = await Category.findByPk(body.categoryId)
+        if (category == null) {
+            return json({ message: `Category: ${body.categoryId} not found.` }, 404);
+        }
+        associate.category = category;
+    }
+
     const post = await Post.create(body);
 
     return json(post);
