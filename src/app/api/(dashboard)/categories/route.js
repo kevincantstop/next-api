58d4491bc6624d29316af5c7234b16997607e988
kevin <kevincantstop@gmail.com>
2024-09-29 09:53:10 +0800

    fixed issues

--- src/app/api/(dashboard)/categories/route.js
+++ src/app/api/(dashboard)/categories/route.js
@@ -2,12 +2,18 @@ import { json } from "@/core";
 import { Category } from "@/core/models";
 
 export const GET = async () => {
-    const categories = await Category.findAll()
-    return json(categories)
+    const categories = await Category.findAll();
+    return json(categories);
 }
 
 export const POST = async (req) => {
     const body = await req.json();
-    const category = await Category.create(body)
-    return json(category)
+
+    const categoryBySlug = await Category.findOne({ where: { slug: body.slug } });
+    if (categoryBySlug) {
+        return json({ message: `Category has the same slug not allowed` }, 401);
+    }
+
+    const category = await Category.create(body);
+    return json(category);
 }
--- src/app/api/(dashboard)/posts/route.js
+++ src/app/api/(dashboard)/posts/route.js
@@ -10,10 +10,10 @@ export const POST = async (req) => {
     const { categories, data } = await req.json();
 
     if (!data.authorId) {
-        return json({ message: `AuthorId is empty`, ok: false })
+        return json({ message: `AuthorId is empty`, ok: false }, 401)
     }
     if (!categories || categories.length === 0) {
-        return json({ message: `Categories not assigned`, ok: false })
+        return json({ message: `Categories not assigned`, ok: false }, 401)
     }
 
     const user = await User.findByPk(data.authorId);
@@ -24,6 +24,11 @@ export const POST = async (req) => {
     const cats = (await Promise.all(categories.map(Category.findByPk))).filter(c => c);
     if (cats.length === 0) {
         return json({ message: `No categories found` }, 404);
+    }
+
+    const postBySlug = await Post.findOne({ where: { slug: params.slug } });
+    if (postBySlug) {
+        return json({ message: `Post has the same slug not allowed` }, 401);
     }
 
     const post = await Post.create(data);
